36f427909044f40245dab4d89ece26ae6a6e3afa
Junwon Lee <cpprhtn@naver.com>
2025-02-26 16:35:50 +0900

    feat: markdown 기능 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import tsconfigPaths from "vite-tsconfig-paths";
+import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@components": path.resolve(__dirname, "src/components"),
+    },
+  },
+  optimizeDeps: {
+    include: ["react-markdown", "remark-gfm"],
+  },
 });
