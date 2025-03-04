188c374e14b87782991c8dc9d267b4e78431096a
Junwon Lee <cpprhtn@naver.com>
2025-03-04 20:21:24 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/SUSC-WEB-FE/",
+  base: "/",
   optimizeDeps: {
     include: ["react-markdown", "remark-gfm"],
   },
