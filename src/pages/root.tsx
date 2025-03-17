435c37a1fa429b093314491b5620754bc44bca11
Junwon Lee <cpprhtn@naver.com>
2025-03-17 21:39:25 +0900

    feat: admin page

--- src/pages/root.tsx
+++ src/pages/root.tsx
@@ -1,16 +1,44 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import devtools from "devtools-detect";
 
 const Root = () => {
+  const [devToolsOpen, setDevToolsOpen] = useState(false);
+
+  useEffect(() => {
+    const detectDevTools = () => {
+      if (devtools.isOpen) {
+        setDevToolsOpen(true);
+        return;
+      }
+
+      if (window.outerWidth - window.innerWidth > 160) {
+        setDevToolsOpen(true);
+      } else {
+        setDevToolsOpen(false);
+      }
+    };
+
+    window.addEventListener("resize", detectDevTools);
+
+    return () => {
+      window.removeEventListener("resize", detectDevTools);
+    };
+  }, []);
+
   return (
     <>
       <h1>SUSC 설문모음</h1>
       <ul>
-        {/* <li>
-          <Link to="/ParticipationSurvey">참여 설문</Link>
-        </li> */}
         <li className="survey-link">
           <Link to="/DemandSurvey">수요 조사 참여하기</Link>
         </li>
+
+        {devToolsOpen && (
+          <li className="survey-link">
+            <Link to="/admin">관리자 페이지</Link>
+          </li>
+        )}
       </ul>
     </>
   );
